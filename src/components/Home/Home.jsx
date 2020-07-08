12bc3181f6963f92001c9d5af2a6c0a9cf50cec8
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-07-07 20:41:16 -0700

    home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Home.css';
+
+const Home = () => {
+
+
+  return (
+    <section className={styles.Home}>
+      <h1>Home</h1>
+    </section>
+  );
+};
+
+export default Home;
