2568d289a526d87bc91f452952bd41cf1ff16bd8
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-07-08 22:32:38 -0700

    added data

---
+++ data/data.jsx
@@ -0,0 +1,36 @@
+const projectData = [
+  {
+    name: 'Alcherithm',
+    img: '',
+    github: '',
+    site: '',
+    languages: '',
+    description: ''
+  },
+  {
+    name: 'Sew Organized',
+    img: '',
+    github: '',
+    site: '',
+    languages: '',
+    description: ''
+  },
+  {
+    name: 'Let/\'s Talk About It',
+    img: '',
+    github: '',
+    site: '',
+    languages: '',
+    description: ''
+  },
+  {
+    name: 'TBD',
+    img: '',
+    github: '',
+    site: '',
+    languages: '',
+    description: ''
+  }
+];
+
+export default projectData;
---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Project.css';
+
+const Project = () => {
+
+
+  return (
+    <section className={styles.Project}>
+      <h1>Home</h1>
+    </section>
+  );
+};
+
+export default Project;
