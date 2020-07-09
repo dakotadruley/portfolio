4a897ffad54801c4bb431c442f6e4e33300f3ac3
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-07-08 22:46:30 -0700

    added project component

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import projectData from '../../../data/data.jsx';
 import styles from './Project.css';
 
 const Project = () => {
 
-
   return (
     <section className={styles.Project}>
-      <h1>Home</h1>
+      <h1>{projectData.name}</h1>
+      <img src={projectData.img} alt={projectData.name}/>
+      <h4>{projectData.github}</h4>
+      <h4>{projectData.site}</h4>
+      <h3>{projectData.languages}</h3>
+      <p>{projectData.description}</p>
     </section>
   );
 };
