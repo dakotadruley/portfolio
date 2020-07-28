775e3b973a661623ab2bd23c54b7040987c1d9f0
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-07-28 13:53:54 -0700

    added some data

--- data/data.jsx
+++ data/data.jsx
@@ -5,7 +5,7 @@ const projectData = [
     github: '',
     site: '',
     languages: '',
-    description: ''
+    description: 'Junior software developers often struggle with the learning process of code challenges. This learning platform combines resources, videos and documentation to help developers build a foundation. Users can not only practice from alibrary of code challenges, but they can also review past attempts at various problem.'
   },
   {
     name: 'Sew Organized',
@@ -13,7 +13,7 @@ const projectData = [
     github: '',
     site: '',
     languages: '',
-    description: ''
+    description: 'A virtual repository for storing thread based on color and serial number so that any cross stitcher can have an organized stash.'
   },
   {
     name: 'Let/\'s Talk About It',
@@ -21,15 +21,15 @@ const projectData = [
     github: '',
     site: '',
     languages: '',
-    description: ''
+    description: 'A bot that that helps promote the destigmatisation of mental health by sharing facts and questions to help spark conversation and reflection.'
   },
   {
-    name: 'TBD',
+    name: 'Alchimia',
     img: '',
     github: '',
     site: '',
     languages: '',
-    description: ''
+    description: 'A tile based online board game. Score is kept based on how many and what kind of connections were made.'
   }
 ];
 
